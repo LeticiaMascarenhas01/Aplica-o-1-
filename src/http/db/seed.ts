e9e1c061d8b160d1ef4f0aaf126bb6138257d8aa
LeticiaMascarenhas01 <lsmascarenhas20@gmail.com>
2024-09-14 19:46:13 -0300

    Adiição de caracteres

--- src/http/db/seed.ts
+++ src/http/db/seed.ts
@@ -1,14 +1,25 @@
 import { db } from "./index"
 import { goalCompletions, goals} from "./schema"
+import daysjs from "dayjs" 
 
 async function seed() {
     await db.delete(goalCompletions)
     await db. delete(goals)
 
-    await db.insert(goals).values([
+    const result = await db
+    .insert(goals)
+    .values([
         {title: "Acordar cedo", desireWeekflyFrequency: 5},
         {title: "Me exercitar", desireWeekflyFrequency: 3},
         {title: "Meditar", desireWeekflyFrequency: 1},
+    ])
+    .returning()
+
+    const startOfweeK = daysjs().startOf("week")
+
+    await db.insert(goalCompletions).values([
+        {goalId: result[0].id, createdAt: startOfweek.toDate()},
+        {goalId: result[1].id, createdAt: startOfweek.add(1."day").toDate()},
     ])
 }
 
---
+++ src/http/functions/created-goals.js
@@ -0,0 +1,20 @@
+import { title } from "process"
+
+const CreatGoalRequeste {
+    desireWeekFrequency: number
+} 
+
+export async function CreatGoalR({title, desireWeekFrequency}: CreatGoalRequeste)}
+await db.insert(goals).value({
+    title,
+    desireWeekFrequency,
+  )}
+  .returne()
+
+  const goal = result[0]
+
+  return {
+    goal.
+     
+  }
+}
